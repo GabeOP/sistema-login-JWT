d394a534f69b25c0283f52266d9655626a2050fc
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-16 16:49:07 -0300

    adiciona rota privada

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,26 +5,28 @@ const jwt = require("jsonwebtoken");
 
 const Controller = {
   post: async (req, res) => {
-    const { email, senha, confirmaSenha } = req.body;
-
-    if (!email || !senha || !confirmaSenha) {
-      return res.status(422).json({ message: "Preencha todos os campos!" });
-    }
-
-    if (senha !== confirmaSenha) {
-      return res.status(422).json({ message: "As senhas devem ser idênticas" });
-    }
-
-    if (!validator.isEmail(email)) {
-      return res.status(422).json({ message: "Insira um e-mail válido" });
-    }
-
-    const userExiste = await User.findOne({ email });
-    if (userExiste) {
-      return res.status(422).json({ message: "Usuário já cadastrado" });
-    }
-
     try {
+      const { email, senha, confirmaSenha } = req.body;
+
+      if (!email || !senha || !confirmaSenha) {
+        return res.status(422).json({ message: "Preencha todos os campos!" });
+      }
+
+      if (senha !== confirmaSenha) {
+        return res
+          .status(422)
+          .json({ message: "As senhas devem ser idênticas" });
+      }
+
+      if (!validator.isEmail(email)) {
+        return res.status(422).json({ message: "Insira um e-mail válido" });
+      }
+
+      const userExiste = await User.findOne({ email });
+      if (userExiste) {
+        return res.status(422).json({ message: "Usuário já cadastrado" });
+      }
+
       const senhaEncriptada = await bcrypt.hash(senha, 10);
 
       const user = new User({ email, senha: senhaEncriptada });
@@ -35,32 +37,49 @@ const Controller = {
       res.status(500).json({ message: "Ocorreu um erro interno." });
     }
   },
-  login: async(req,res)=>{
-    const {email, senha} = req.body;
-  
-    if(!email || !senha){
-      return res.status(422).json({ message: "Preencha todos os campos!" });
-    }
-  
-    if (!validator.isEmail(email)) {
-      return res.status(422).json({ message: "Insira um e-mail válido" });
-    }
-    
-    const user = await User.findOne({email})
-    if(!user){
-      return res.status(404).json({message: "Usuário não encontrado"})
-    }
-  
-    const comparaSenha = await bcrypt.compare(senha, user.senha);
+  login: async (req, res) => {
+    try {
+      const { email, senha } = req.body;
 
-    if(!comparaSenha){
-      return res.status(422).json({message: "Senha incorreta"})
+      if (!email || !senha) {
+        return res.status(422).json({ message: "Preencha todos os campos!" });
+      }
+
+      const emailValido = validator.isEmail(email);
+      if (!emailValido) {
+        return res.status(422).json({ message: "Insira um e-mail válido" });
+      }
+
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: "E-mail não cadastrado" });
+      }
+
+      const comparaSenha = await bcrypt.compare(senha, user.senha);
+      if (!comparaSenha) {
+        return res.status(404).json({ message: "Senha incorreta" });
+      }
+      console.log(user);
+
+      const token = jwt.sign({ email }, process.env.SECRET_KEY, {
+        expiresIn: 86400,
+      });
+
+      res.status(200).json({ message: "Login realizado com sucesso!", token });
+    } catch (error) {
+      res.status(500).json({ message: "Ocorreu um erro. " + error });
+    }
+  },
+  user: async (req, res) => {
+    const { email } = req.params;
+
+    const user = await User.findOne({ email }, "-senha");
+    if (!user) {
+      return res.status(404).json({ msg: "Usuário não encontrado" });
     }
 
-    const token = jwt.sign({email}, "123", {expiresIn: '1h'})
-
-    res.status(200).json({message: "Login realizado com sucesso!", token})
-  }
+    res.status(200).json(user);
+  },
 };
 
 module.exports = Controller;
--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const dbConnect = require("./db/db");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 //Conexão com o banco de dados
 dbConnect();
 
 const Controller = require("./controllers/user.controller");
+const checaToken = require("./middleware/auth");
 
-app.post("/login", Controller.login)
-app.post("/cadastro", Controller.post)
+app.post("/login", Controller.login);
+app.post("/cadastro", Controller.post);
+app.get("/user/:email", checaToken, Controller.user);
 
-app.listen(3000, ()=> console.log("Rodando servidor na porta 3000"))
+app.listen(3000, () => console.log("Rodando servidor na porta 3000"));
